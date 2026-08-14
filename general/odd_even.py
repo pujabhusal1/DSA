84b9658a596249f0398685647e08267dfa08e550
pujabhusal1 <pujabhusal1234@gmail.com>
2026-08-14 22:12:57 +0545

    odd_even

---
+++ general/odd_even.py
@@ -0,0 +1,16 @@
+"""""
+Add a program that returns 1 if odd and 0 if even. 
+Assume inputs are from 0-50000. 
+Also add unit tests to cover all unit tests ( both boundary and variety)
+"""""
+def odd_even(num):
+  if num % 2 != 0:
+     return 1
+  else:
+     return 0 
+assert(odd_even(5000) == 0) 
+assert(odd_even(8001) == 1)
+assert(odd_even(10500)== 0)
+assert(odd_even(20450) == 0)
+assert(odd_even(35651) == 1 )
+
