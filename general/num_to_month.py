f1befe8b9b5d11cc990de7a4f747b1fe125d3c9e
pujabhusal1 <pujabhusal1234@gmail.com>
2026-08-15 14:38:51 +0545

    dict

--- general/num_to_month.py
+++ general/num_to_month.py
@@ -1,4 +1,4 @@
-def get_month_name (num):
+'''def get_month_name (num):
     if num == 1:
         return "january"
     elif num == 2:
@@ -28,5 +28,50 @@ assert(get_month_name(1) == "january")
 assert(get_month_name(4)) == "april"
 assert(get_month_name(8)) == "august"
 assert(get_month_name(12)) == "december"
+
+if there is any invalid number, then return "invalid" 
+Hint : use dictionariy's "not in" feature and also update 
+unit test
+''' 
+'''def get_month_name (num):
+    num = {1: "january", 2: "februry", 3: "march", 4: "april", 5: "may", 6: "june", 7: "july", 8: "august"}
+    return num
+assert(get_month_name(4) == "april")'''
+
+
+
+
+
+'''Dictonary'''
+
+def get_month_name(num):
+
+    months = {
+        1: "january",
+        2: "february",
+        3: "march",
+        4: "april",
+        5: "may",
+        6: "june",
+        7: "july",
+        8: "august",
+        9: "september",
+        10: "october",
+        11: "november",
+        12: "December",
+    }
+
+    if num not in months:
+        print("invalid")
+        return "invalid"
+    else: 
+        print(months[num])
+        return months[num]
+
+assert get_month_name(1) == "january"
+assert get_month_name(4) == "april"
+assert get_month_name(9) == "september"
+assert get_month_name(13) == "invalid"
+
 
     
