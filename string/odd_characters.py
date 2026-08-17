603178b5b6aa568f9bfb6e789058d980b14be31c
pujabhusal1 <pujabhusal1234@gmail.com>
2026-08-17 21:14:52 +0545

    odd position string printed

---
+++ string/odd_characters.py
@@ -0,0 +1,23 @@
+"""
+Create a new string from characters in odd position only. First one is 0th position. 
+
+"abc" -> "b"
+"abcdef" -> "bdf" 
+
+Assume : Max string size is 20.
+"""
+def odd_string(mystring):
+    ans = ""
+    for i in range(len(mystring)):
+       # print(mystring[i])    
+        if i % 2 == 1:
+            ans = ans + mystring[i]
+            #print(ans)
+        #else:
+            #print('no')
+    return ans
+(odd_string('bhusal'))
+assert(odd_string('') == '')
+assert(odd_string('puja') == 'ua' )
+assert(odd_string('kanxabhusal') == 'axbua')
+assert(odd_string('123456puja1234') == '246ua24')
