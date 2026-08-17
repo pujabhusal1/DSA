487ad5ce332ba79f5b8dfd785fe651b1593b42b9
pujabhusal1 <pujabhusal1234@gmail.com>
2026-08-17 18:51:12 +0545

    updated unit test

--- string/reverse_string.py
+++ string/reverse_string.py
@@ -6,18 +6,22 @@ Example : "abc" -> "cba"
 "" -> ""
 "a" -> "a" 
 
-Hint : Use stack
+Hint : Use stack 
 """
 
 def reverse_string(mystring):
     ans = ""
-    for i in mystring:
-        #print(i)
-        ans = i + ans
+    for char in mystring:
+        #print(char)
+        ans = char + ans
     return ans
 (reverse_string('puja'))
 
-assert(reverse_string('bhusal'))=='lasuhb'
-assert(reverse_string('') == '')
+assert reverse_string('bhusal') == 'lasuhb'
+assert reverse_string('') == ''
+assert reverse_string('a') == 'a'
+assert reverse_string('puja bhusal') == "lasuhb ajup"
+assert reverse_string('1234567890123456789') == "9876543210987654321"
+assert reverse_string('aba') == "aba"
 
 
