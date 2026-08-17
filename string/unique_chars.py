8495d1212c3d712c6882a02cf88ca5fdae8fa12e
pujabhusal1 <pujabhusal1234@gmail.com>
2026-08-17 18:57:56 +0545

    modified

--- string/unique_chars.py
+++ string/unique_chars.py
@@ -8,17 +8,19 @@ Number of unique characters in a string : Assume string size <= 20.
 Hint : Use set.
 """
 def unique_chars(string):
-    my_set = set()
-    if len(string) > 20:
-        return 0
+    my_set = set() # set helps to reduce repetation
+    if len(string) >= 20:
+        return -1
     for char in string:
         my_set.add(char) 
     return len(my_set)
-#print(unique_chars("pujaaaaaaa"))
+print(unique_chars("pujaaaaaaa"))
 
-assert(unique_chars('44444') == 1)
+'''assert(unique_chars('') == 0)
 assert(unique_chars('a') ==1)
+assert(unique_chars('44444') == 1)
 assert(unique_chars('sangansangan') == 4)
 assert(unique_chars('as11as11sss') == 3)
-assert(unique_chars('nasjdmjdjjdjjdjdjdjjdjdjd') == 0)
+assert(unique_chars('nasjdmjdjjdjjdjdjdjjdjdjd') == -1)
+'''
 
